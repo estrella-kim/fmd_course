0d66ed45efe17a447f8910280cc7db9bf1b2d23e
estrella-kim <puris38@naver.com>
2018-01-03 01:05:59 +0900

    오류 수정

--- react_course/src/components/Contents.js
+++ react_course/src/components/Contents.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Contents.css'
+import Counter from '../containers/Counter';
 
 class Contents extends React.Component{
     render() {
-        console.log(this)
         return(
-
             <div>
                 {this.props.children}
                 { 1+ 6 }
                 컨텐츠입니다.
+                <Counter />
             </div>
         )
 
--- react_course/src/containers/Counter.js
+++ react_course/src/containers/Counter.js
@@ -38,20 +38,16 @@ class Counter extends React.Component{
         return false;
     }
     render() {
-        return(
-            // <!-- 최상위는 div하나만 존재하고
-            <div className="container">
-                <div>{this.state.number}</div>
-                <button onClick={() => this.increase()}>증가</button> {{/*리액트 문법상 onClick*/}}
+        return (
+            <div className="counter">
+                { /*최상위는 div하나만 존재하고*/ }
+                <div>{ this.state.number }</div>
+                <button onClick={() => this.increase()}>증가</button> {/*리액트 문법상 onClick*/}
                 <button disabled={!this.state.number} onClick={() => this.decrease()}>감소</button>
-                <div > { this.state.number<=0 &&  '- 0미만으로는 내려갈수없어요.'}</div>
-                {
-                    this.state.nmber <= 0 && <div>0 미만으로는 내려갈 수 없습니다.</div> //어떤 조건일 때에만 이걸 해라라는 걸 할때는 && 혹은 ||
-                }
+                <div > { this.state.number <= 0 &&  '- 0미만으로는 내려갈수없어요.'}</div>
+                {/*{this.state.number <= 0 && <div>0 미만으로는 내려갈 수 없습니다.</div>  /*어떤 조건일 때에만 이걸 해라라는 걸 할때는 && 혹은 ||}}*/}
             </div>
-
         )
-
     }
 }
 
