ce5c538960ef2dc49f8b5c7105d597072cd0347c
estrella-kim <puris9030@gmail.com>
2018-01-17 18:39:29 +0900

    babel과 eslint setting

---
+++ env/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends" : "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion" : 6
+    },
+    "env" : {
+        "browser" : true
+    },
+    "rules" : {
+        "padded-blocks": 1,
+        "no-console" : 1
+    }
+};
---
+++ env/index.js
@@ -0,0 +1,9 @@
+class Car {
+
+    excel() {
+        console.log('excel')
+    }
+}
+
+let avante = new Car;
+avante.excel();
