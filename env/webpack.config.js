19e0376f2bd1b6cbdadb4395e939a06dcb2cfec4
estrella-kim <puris9030@gmail.com>
2018-01-23 19:03:48 +0900

    loader related to style  setting in webpack

--- env/webpack.config.js
+++ env/webpack.config.js
@@ -27,6 +27,14 @@ module.exports = {
                 test : /\.js$/,
                 exclude : /node_modules/,
                 loader : 'eslint-loader'
+            },
+            {
+                test : /\.css$/,
+                exclude : /node_modules/,
+                use : [
+                    { loader : 'style-loader'},
+                    { loader : 'css-loader'}
+                ]
             }
         ]
     }
