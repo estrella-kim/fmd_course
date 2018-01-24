d544a9f66867552c511e5669ea22967596de4420
estrella-kim <puris9030@gmail.com>
2018-01-24 15:21:17 +0900

    리스트 기능 추가

--- env/src/components/App.js
+++ env/src/components/App.js
@@ -7,8 +7,10 @@ export class App extends React.Component{
         super();
         this.state = {
             text : '',
-            textLength : 0
+            textLength : 0,
+            list : []
         }
+
     }
     getTextLength (e) {
         const text = e.target.value;
@@ -16,15 +18,30 @@ export class App extends React.Component{
         if(textLength > 15) return;
         this.setState({text, textLength})
     }
+
+    recordList(e) {
+        e.preventDefault();//form 태그의 preventDefault 는 제출 후의 페이지 갱신을 막는다.
+        this.setState( state => {
+            return {
+                list: [...state.list, state.text],
+                text: ''
+            }
+        });
+    }
+
     render() {
 
         return (
             <div>
-                hi, this is app
                 <Board/>
                 <div> { this.state.textLength}/ 15 </div>
                 { this.state.textLength == 15  && <div>15자 이상을 넘을 수 없습니다.</div>}
+            <form onSubmit={(e)=>this.recordList(e)}>
                 <Input onChange={(e) => this.getTextLength(e) } value={this.state.text}/>
+            </form>
+                <ul>
+                    { this.state.list.map((v,i) => (<li key={i}>{v}</li>)) }
+                </ul>
             </div>
         );
     }
